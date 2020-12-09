36cb8345284da619107b0470c59462293b13f2b6
yashu <yashgeddada@gmail.com>
2020-12-09 19:39:44 +0530

    changes in data

---
+++ Models/orders.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+namespace ECommerce_Application
+{
+    class orders
+    {
+        public int prodId { get; set; }
+        public int UserId { get; set; }
+        public int quantity { get; set; }
+
+        public orders()
+        {
+
+        }
+
+        public orders(int prodId, int userId, int quantity)
+        {
+            this.prodId = prodId;
+            this.UserId = userId;
+            this.quantity = quantity;
+        }
+        //int prodId, int custId, int quantity
+        
+    }
+}
